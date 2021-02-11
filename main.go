bf7be8653d71b3c77087160ccad335f709d53133
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2021-02-11 16:39:46 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+	"internal/httpserver"
 )
 
 func main() {
-	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
-		time.Sleep(10 * time.Second)
-		fmt.Fprintf(responseWriter, "Welcome to my website!")
-	})
-	http.ListenAndServe(":4080", nil)
+	 httpserver.Launch()
 }
